7e9c5e8640b99d4dee05601f35fc4dcc3cc778e6
Dmytro Demko <dmytro.d2@gso.schule.koeln>
2023-03-08 12:40:07 +0100

    Fast fertig

--- Immobilienrechner/Program.cs
+++ Immobilienrechner/Program.cs
@@ -9,7 +9,7 @@ do
     Console.WriteLine("Bitte wählen Sie den passenden Rechner für Sie");
     Console.WriteLine("1 - Kaufkostenrechner");
     Console.WriteLine("2 - Mietrenditenrechner (Brutto)");
-    Console.WriteLine("3 - Baufinanzierungsrechner");
+    Console.WriteLine("3 - Kreditlaufzeitrechner");
     Console.WriteLine("4 - Zurück zum Sub-Menü");
     Console.WriteLine("5 - Exit--> Programm beenden");
     auswahlmenü = Console.ReadLine();
@@ -31,12 +31,49 @@ do
         
         double Marklergeld = Marklerkostendouble / 100 * Kaufpreisdouble  ;
         double Gesamt = Kaufpreisdouble + Notarbetrag + Grundwerbesteuer + Marklergeld;
-        Console.WriteLine($"Ihr Gesamtpreis beträgt:{Gesamt}");
+        Console.WriteLine($"Ihr Gesamtpreis beträgt:{Gesamt}EUR");
 
         Console.ReadLine();
 
     }
+    else if  (auswahlmenü == "2")
+    {
+        Console.Clear();
+        Console.WriteLine("Mietrenditenrechner");
+        Console.WriteLine("Geben Sie die Jahreskaltmiete ein:");
 
+        string Jahreskaltmiete = Console.ReadLine();
+        double mietedouble = Convert.ToDouble(Jahreskaltmiete);
 
+        Console.WriteLine("Geben Sie den Kaufpreis der Immobilie ein:");
+        string immo = Console.ReadLine();
+        double immodouble = Convert.ToDouble(immo);
+
+        double rendite = mietedouble / immodouble * 100;
+        Console.WriteLine($"Ihr Mietrendite beträgt {rendite}%");
+        Console.ReadLine();
+
+    }
+    else if (auswahlmenü == "3")
+    {
+        Console.Clear();
+        Console.WriteLine("Kreditlaufzeitrechner");
+        Console.WriteLine("Geben Sie die Kreditsumme ein:");
+        string Ksumme = Console.ReadLine();
+        double ksdouble = Convert.ToDouble(Ksumme);
+        Console.WriteLine("Geben Sie ihre Zinsen ein:");
+        string zsumme = Console.ReadLine();
+        double zsdouble = Convert.ToDouble(zsumme);
+        Console.WriteLine("Geben Sie den Zinssatz in Prozent ein:");
+        string psumme = Console.ReadLine();
+        double psdouble = Convert.ToDouble(psumme);
+        double t = zsdouble * 100 * 360 / (ksdouble * psdouble);
+        Console.WriteLine($"Ihre Kreditlaufzeit liegt bei {t} Monaten");
+        Console.ReadLine();
+    }
+    else if (auswahlmenü == "5")
+    {
+        Environment.Exit(0);
+    }
 }
 while (auswahlmenü != "4" || auswahlmenü != "5"); 
