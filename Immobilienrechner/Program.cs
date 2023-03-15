cf68b73950e62c2b420d0ccd947b652ccbd065f3
Dmytro Demko <dmytro.d2@gso.schule.koeln>
2023-03-15 12:18:53 +0100

    .

--- Immobilienrechner/Program.cs
+++ Immobilienrechner/Program.cs
@@ -1,79 +1,85 @@
-﻿
-string auswahlmenü;
-do
-    
+﻿try
 {
-    Console.Clear();
-    Console.WriteLine("Immobilienrechner");
-    Console.WriteLine("Menü");
-    Console.WriteLine("Bitte wählen Sie den passenden Rechner für Sie");
-    Console.WriteLine("1 - Kaufkostenrechner");
-    Console.WriteLine("2 - Mietrenditenrechner (Brutto)");
-    Console.WriteLine("3 - Kreditlaufzeitrechner");
-    Console.WriteLine("4 - Zurück zum Sub-Menü");
-    Console.WriteLine("5 - Exit--> Programm beenden");
-    auswahlmenü = Console.ReadLine();
-    if (auswahlmenü == "1")
+    string auswahlmenü;
+    do
+
     {
         Console.Clear();
-        Console.WriteLine("Kaufkostenrechner");
-        Console.WriteLine("Geben Sie den Kaufpreis der Immobilie ein:");
-        string Kaufpreisvar = Console.ReadLine();
-        double Kaufpreisdouble = Convert.ToDouble(Kaufpreisvar);
-        
-        Console.WriteLine("Geben Sie die Marklerkosten in % ein");
-        string Marklerkostenvar = Console.ReadLine();
-        double Marklerkostendouble = Convert.ToDouble(Marklerkostenvar);
+        Console.WriteLine("Immobilienrechner");
+        Console.WriteLine("Menü");
+        Console.WriteLine("Bitte wählen Sie den passenden Rechner für Sie");
+        Console.WriteLine("1 - Kaufkostenrechner");
+        Console.WriteLine("2 - Mietrenditenrechner (Brutto)");
+        Console.WriteLine("3 - Kreditlaufzeitrechner");
+        Console.WriteLine("4 - Zurück zum Sub-Menü");
+        Console.WriteLine("5 - Exit--> Programm beenden");
+        auswahlmenü = Console.ReadLine();
+        if (auswahlmenü == "1")
+        {
+            Console.Clear();
+            Console.WriteLine("Kaufkostenrechner");
+            Console.WriteLine("Geben Sie den Kaufpreis der Immobilie ein:");
+            string Kaufpreisvar = Console.ReadLine();
+            double Kaufpreisdouble = Convert.ToDouble(Kaufpreisvar);
 
-        double Notarbetrag = Kaufpreisdouble * 0.015;
-        
-        double Grundwerbesteuer = Kaufpreisdouble * 0.065;
-        
-        double Marklergeld = Marklerkostendouble / 100 * Kaufpreisdouble  ;
-        double Gesamt = Kaufpreisdouble + Notarbetrag + Grundwerbesteuer + Marklergeld;
-        Console.WriteLine($"Ihr Gesamtpreis beträgt:{Gesamt}EUR");
+            Console.WriteLine("Geben Sie die Marklerkosten in % ein");
+            string Marklerkostenvar = Console.ReadLine();
+            double Marklerkostendouble = Convert.ToDouble(Marklerkostenvar);
 
-        Console.ReadLine();
+            double Notarbetrag = Kaufpreisdouble * 0.015;
 
+            double Grundwerbesteuer = Kaufpreisdouble * 0.065;
+
+            double Marklergeld = Marklerkostendouble / 100 * Kaufpreisdouble;
+            double Gesamt = Kaufpreisdouble + Notarbetrag + Grundwerbesteuer + Marklergeld;
+            Console.WriteLine($"Ihr Gesamtpreis beträgt:{Gesamt}EUR");
+
+            Console.ReadLine();
+
+        }
+        else if (auswahlmenü == "2")
+        {
+            Console.Clear();
+            Console.WriteLine("Mietrenditenrechner");
+            Console.WriteLine("Geben Sie die Jahreskaltmiete ein:");
+
+            string Jahreskaltmiete = Console.ReadLine();
+            double mietedouble = Convert.ToDouble(Jahreskaltmiete);
+
+            Console.WriteLine("Geben Sie den Kaufpreis der Immobilie ein:");
+            string immo = Console.ReadLine();
+            double immodouble = Convert.ToDouble(immo);
+
+            double rendite = mietedouble / immodouble * 100;
+            Console.WriteLine($"Ihr Mietrendite beträgt {rendite}%");
+            Console.ReadLine();
+
+        }
+        else if (auswahlmenü == "3")
+        {
+            Console.Clear();
+            Console.WriteLine("Kreditlaufzeitrechner");
+            Console.WriteLine("Geben Sie die Kreditsumme ein:");
+            string Ksumme = Console.ReadLine();
+            double ksdouble = Convert.ToDouble(Ksumme);
+            Console.WriteLine("Geben Sie ihre Zinsen ein:");
+            string zsumme = Console.ReadLine();
+            double zsdouble = Convert.ToDouble(zsumme);
+            Console.WriteLine("Geben Sie den Zinssatz in Prozent ein:");
+            string psumme = Console.ReadLine();
+            double psdouble = Convert.ToDouble(psumme);
+            double t = zsdouble * 100 * 360 / (ksdouble * psdouble);
+            Console.WriteLine($"Ihre Kreditlaufzeit liegt bei {t} Monaten");
+            Console.ReadLine();
+        }
+        else if (auswahlmenü == "5")
+        {
+            Environment.Exit(0);
+        }
     }
-    else if  (auswahlmenü == "2")
-    {
-        Console.Clear();
-        Console.WriteLine("Mietrenditenrechner");
-        Console.WriteLine("Geben Sie die Jahreskaltmiete ein:");
-
-        string Jahreskaltmiete = Console.ReadLine();
-        double mietedouble = Convert.ToDouble(Jahreskaltmiete);
-
-        Console.WriteLine("Geben Sie den Kaufpreis der Immobilie ein:");
-        string immo = Console.ReadLine();
-        double immodouble = Convert.ToDouble(immo);
-
-        double rendite = mietedouble / immodouble * 100;
-        Console.WriteLine($"Ihr Mietrendite beträgt {rendite}%");
-        Console.ReadLine();
-
-    }
-    else if (auswahlmenü == "3")
-    {
-        Console.Clear();
-        Console.WriteLine("Kreditlaufzeitrechner");
-        Console.WriteLine("Geben Sie die Kreditsumme ein:");
-        string Ksumme = Console.ReadLine();
-        double ksdouble = Convert.ToDouble(Ksumme);
-        Console.WriteLine("Geben Sie ihre Zinsen ein:");
-        string zsumme = Console.ReadLine();
-        double zsdouble = Convert.ToDouble(zsumme);
-        Console.WriteLine("Geben Sie den Zinssatz in Prozent ein:");
-        string psumme = Console.ReadLine();
-        double psdouble = Convert.ToDouble(psumme);
-        double t = zsdouble * 100 * 360 / (ksdouble * psdouble);
-        Console.WriteLine($"Ihre Kreditlaufzeit liegt bei {t} Monaten");
-        Console.ReadLine();
-    }
-    else if (auswahlmenü == "5")
-    {
-        Environment.Exit(0);
-    }
+    while (auswahlmenü != "4" || auswahlmenü != "5");
 }
-while (auswahlmenü != "4" || auswahlmenü != "5"); 
+catch (System.FormatException ex_format)
+{
+    Console.WriteLine("!!!Sie haben einen Fehler gemacht, versuchen Sie es erneut!!!"); }
+
